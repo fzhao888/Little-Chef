4de3b2705cd02b9832c90b91b3291049816db11e
fzhao888 <frank.zhao93@gmail.com>
2023-08-10 20:05:55 -0500

    added seeds/index

--- seeds/index.js
+++ seeds/index.js
@@ -1,4 +1,36 @@
- const seedRecipeIngredients = require('./recipe-ingredient-seeds');
- const seedRecipes = require('./recipe-seeds');
- const seedIngredients = require('./ingredients-seeds');
- const se
+const seedRecipeIngredients = require("./recipe-ingredient-seeds");
+const seedRecipes = require("./recipe-seeds");
+const seedIngredients = require("./ingredients-seeds");
+const seedRecipeIngredients = require("./recipe-ingredient-seeds");
+const seedUsers = require("./user-seeds");
+const seedUserRecipes = require("./user-recipe-seeds");
+const seedPosts = require("./post-seeds");
+
+const sequelize = require("../config/connection");
+
+const seedAll = async () => {
+  await sequelize, sync({ force: true });
+  console.log("\n----- DATABASE SYNCED -----\n");
+  
+  await seedRecipes();
+  console.log("\n----- RECIPES SEEDED -----\n");
+
+  await seedRecipeIngredients();
+  console.log("\n----- RECIPE INGREDIENTS SEEDED -----\n");
+
+  await seedIngredients();
+  console.log("\n----- INGREDIENTS SEEDED -----\n");
+
+  await seedUsers();
+  console.log("\n----- USERS SEEDED -----\n");
+
+  await seedUserRecipes();
+  console.log("\n-----  USER RECIPES SEEDED -----\n");
+
+  await seedPosts();
+  console.log("\n-----  POSTS SEEDED -----\n");
+
+  process.exit(0);
+};
+
+seedAll();
