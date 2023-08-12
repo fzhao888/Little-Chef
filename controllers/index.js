4d3ad3d8074c181e3309e043613c0ef7a6b50305
fzhao888 <frank.zhao93@gmail.com>
2023-08-11 19:50:42 -0500

    Added router index

---
+++ controllers/index.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const ingredientRoutes = require('./ingredientRoutes'); 
+const postRoutes = require('./postRoutes');
+const recipesRoutes = require('./recipeRoutes');
+
+router,use('/users', userRoutes);
+router.use('/ingredients',ingredientRoutes);
+router.use('/posts', postRoutes);
+router.use('/recipes',recipesRoutes);
+
+
+module.exports = router;
