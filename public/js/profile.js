bb8642ca5dc85d9da34167726d43f59d72de6cfd
fzhao888 <frank.zhao93@gmail.com>
2023-08-14 12:41:53 -0500

    Added Find New Recipe button to profile

--- public/js/profile.js
+++ public/js/profile.js
@@ -21,8 +21,6 @@ const newFormHandler = async (event) => {
   };
   
   const delButtonHandler = async (event) => {
-    
-    console.log('sssssssssssssssssssssssssssssssssssssssss');
     if (event.target.hasAttribute('data-id')) {
       const id = event.target.getAttribute('data-id');
       const response = await fetch(`/api/ingredients/${id}`, {
