197e324a97940f9795a2974ca5d43bc132006bfa
fzhao888 <frank.zhao93@gmail.com>
2023-08-14 13:29:53 -0500

    Editedd recipe.handlebars - Added recipes header, added logic to display each recipe and add recipe based on id

--- controllers/api/recipeRoutes.js
+++ controllers/api/recipeRoutes.js
@@ -2,4 +2,13 @@ const router = require('express').Router();
 const { Recipe } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+
+
+// renders recipe
+router.get('/', async(req,res) => {
+    res.render('recipe', {
+        logged_in: req.session.logged_in
+    });
+})
+
 module.exports = router;
