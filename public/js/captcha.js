c535d008229b328de4a6229d024f50e79c62cb1d
fzhao888 <frank.zhao93@gmail.com>
2023-08-17 16:47:56 -0500

    Edited to captcha.handlebars and captcha.js for frictionless recaptcha v3

--- public/js/captcha.js
+++ public/js/captcha.js
@@ -1,8 +1,10 @@
 
 function cb(token) {
-    var input = document.createElement('input')
+    let input = document.createElement('input')
     input.setAttribute('type', 'text')
     input.setAttribute('name', 'g-recaptcha-response')
     input.setAttribute('value', token)
     document.getElementsByTagName('form')[0].appendChild(input)
+    document.getElementById('captcha').style.display = 'none';
+    document.getElementById('captcha').submit();
 }
